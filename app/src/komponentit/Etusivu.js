26a38c2d30e9e7bc7b5879d55552e523b942b1a5
Jonpaavo <83001181+Jonpaavo@users.noreply.github.com>
2023-04-24 09:48:14 +0300

    Etusivu valmis?

--- app/src/komponentit/Etusivu.js
+++ app/src/komponentit/Etusivu.js
@@ -7,11 +7,12 @@ const Etusivu = () => {
 
             <Box sx={{p: 5,my: "auto",mx: "auto",textAlign: "center",maxWidth: "800px",}}>
                 <Typography variant="h3" align="center" gutterBottom>Tervetuloa kirja-arkisto sovellukseen!</Typography>
-                <Typography variant="h6" align="left" gutterBottom>Meillä on laaja valikoima kirjoja eri genreistä, jotka odottavat sinua löytämään ne.</Typography>
-                <Typography align="left" gutterBottom>Käyttöliittymämme on suunniteltu helpoksi ja intuitiiviseksi.</Typography>
+                <Typography variant="h6" align="left" gutterBottom>Meillä on laaja valikoima kirjoja eri genreistä, joista voit luoda itsellesi omia kokoelmia!</Typography>
+                <Typography align="left" gutterBottom>Käyttöliittymämme on suunniteltu helpoksi käyttää ja hyödylliseksi omien kokoelmien luontiin.</Typography>
                 <Typography align="left" gutterBottom>Olemme ylpeitä siitä, että voimme tarjota laadukasta lukuelämystä ja auttaa sinua löytämään uusia mielenkiintoisia kirjoja.</Typography>
                 <Typography align="left" gutterBottom>Käytä aikaa selaillaksesi kokoelmaamme, löytääksesi uusia suosikkeja ja luo omia kokoelmia suosikki kirjoistasi!</Typography>
                 <Typography align="left" gutterBottom>Tervetuloa kirja-arkisto sovellukseen ja nauti lukemisesta!</Typography>
+                <Typography align="left" gutterBottom>Alla olevasta napista pääset selaamaan kirja-arkiston kokoelmaa.</Typography>
                     <Box sx={{ display: "flex", justifyContent: "center", mt: 5 }}>
                         <Button variant="contained" color="primary" href="/kokoelma">Tutustu kokoelmaamme</Button>
                     </Box>
