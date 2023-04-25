401c1e92932e58de090199bab0a93baf2f0b4803
Jonpaavo <83001181+Jonpaavo@users.noreply.github.com>
2023-04-25 13:38:59 +0300

    pieni muokkaus

--- app/src/komponentit/Etusivu.js
+++ app/src/komponentit/Etusivu.js
@@ -6,9 +6,10 @@ const Etusivu = () => {
         <Container maxWidth={false} sx={{ bgcolor: "#D4EBEC", minHeight: "100vh", display: "flex" }}>
             <Box sx={{ p: 5, my: "auto", mx: "auto", textAlign: "center", maxWidth: "800px",bgcolor: "#FFFFFF", border: "1px solid #CCCCCC", borderRadius: "10px" }}>
                 <Typography variant="h4" sx={{ mb: 2 }} align="center" gutterBottom>Tervetuloa kirja-arkisto sovellukseen!</Typography>
-                <Typography align="left" gutterBottom>Meillä on laaja valikoima kirjoja eri genreistä, joista voit luoda itsellesi omia kokoelmia!</Typography>
+                <Typography align="left" gutterBottom>Meillä on laaja valikoima kirjoja eri genreistä, joita voit selailla.</Typography>
+                <Typography align="left" gutterBottom>Rekisteröitymisen ja kirjautumisen jälkeen pystyt luomaan omia kokoelmia haluamistasi kirjoista!</Typography>
                 <Typography align="left" gutterBottom>Käyttöliittymämme on suunniteltu helpoksi käyttää ja hyödylliseksi omien kokoelmien luontiin.</Typography>
-                <Typography align="left" gutterBottom>Olemme ylpeitä siitä, että voimme tarjota laadukasta lukuelämystä ja auttaa sinua löytämään uusia mielenkiintoisia kirjoja.</Typography>
+                <Typography align="left" gutterBottom>Olemme ylpeitä siitä, että voimme tarjota sinulle laajan valikoiman kirjoja ja auttaa sinua löytämään sieltä juuri haluamasi kirjat.</Typography>
                 <Typography align="left" gutterBottom>Käytä aikaa selaillaksesi kokoelmaamme, löytääksesi uusia suosikkeja ja luo omia kokoelmia suosikki kirjoistasi!</Typography>
                 <Typography align="left" gutterBottom>Tervetuloa kirja-arkisto sovellukseen ja nauti lukemisesta!</Typography>
                 <Typography align="left" gutterBottom>Alla olevasta napista pääset selaamaan kirja-arkiston kokoelmaa.</Typography>
